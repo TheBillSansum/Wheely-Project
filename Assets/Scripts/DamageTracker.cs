381bfb3849cf384b6b9ac20481cfd5f84e02f487
BillSansu <98390837+BillSansu@users.noreply.github.com>
2023-12-02 21:01:58 +0000

    Menu and finally fixed wheel textures Z issue

--- Assets/Scripts/DamageTracker.cs
+++ Assets/Scripts/DamageTracker.cs
@@ -1,36 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class DamageTracker : MonoBehaviour
 {
-
     public AudioManager audioManager;
 
+    public float DamageTaken = 0;
+    public Image[] AmbientDamageColour;
+    [Range(0f, 100f)] public float percentage = 0f;
+    public TextMeshProUGUI PercentageText;
+
     public float coolDown = 0;
+    public Slider CarImage;
 
     public void OnCollisionEnter(Collision collision)
     {
         if (coolDown <= 0)
         {
-
             if (collision.gameObject.layer == 9)
             {
                 audioManager.PlayDamageClip();
                 coolDown = 5;
-
+                HitWall();
                 Debug.Log("Hit wall");
             }
         }
     }
-    
 
     public void Update()
     {
-        if(coolDown >= 0)
+        CarImage.value = DamageTaken / 100f;
+
+        if (coolDown >= 0)
         {
             coolDown -= Time.deltaTime;
         }
     }
 
+    public void HitWall()
+    {
+        DamageTaken += 10;
+        percentage = DamageTaken;
+
+        percentage = Mathf.Clamp(percentage, 0f, 100f);
+        PercentageText.text = percentage.ToString() + "%";
+        float alpha = percentage / 100f;
+
+        Color newColor = new Color(1f, 0.2235f, 0.2235f, alpha);
+        foreach (Image image in AmbientDamageColour)
+        {
+            image.color = newColor;
+        }
+    }
 }
